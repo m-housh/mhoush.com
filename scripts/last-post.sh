e49f2ac4406f425cd9ec80f05d5907a42cda3d45
Michael Housh <mhoush@houshhomeenergy.com>
2023-12-19 08:59:14 -0500

    Adds last-post script

--- scripts/last-post.sh
+++ scripts/last-post.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 
-echo "$(find content/posts/* -type d -ctime -1 | sort -nr | head -1)"
+# Find the last modified directory in the content/posts directory.
+echo "$(find content/posts/* -type d -ctime -30 | sort -nr | head -1)"
