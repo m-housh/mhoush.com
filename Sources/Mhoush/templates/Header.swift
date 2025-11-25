8af81801f072f72ae356734217eebf9f0fbbf644
Michael Housh <michael@mhoush.com>
2025-11-25 13:16:49 -0500

    fix: Use full url for og:image.

--- Sources/Mhoush/templates/Header.swift
+++ Sources/Mhoush/templates/Header.swift
@@ -23,12 +23,14 @@ func generateHeader(
       meta(content: SiteMetadata.summary, name: "og:description"),
       meta(content: SiteMetadata.twitterImage, name: "og:image"),
       meta(content: "1014", name: "og:image:width"),
-      meta(content: "530", name: "og:image:height")
+      meta(content: "530", name: "og:image:height"),
     ])
-  case let .article(article):
+  case .article(let article):
     return Node.fragment([
       link(href: "/static/prism.css", rel: "stylesheet"),
-      link(href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css", rel: "stylesheet"),
+      link(
+        href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
+        rel: "stylesheet"),
       meta(content: article.summary, name: "description"),
       meta(content: "summary_large_image", name: "twitter:card"),
       meta(content: article.imagePath, name: "twitter:image"),
@@ -36,23 +38,24 @@ func generateHeader(
       meta(content: ogURL(article), name: "og:url"),
       meta(content: article.title, name: "og:title"),
       meta(content: article.summary, name: "og:description"),
-      meta(content: article.imagePath, name: "og:image"),
+      meta(content: ogURL(article), name: "og:image"),
       meta(content: "1014", name: "og:image:width"),
       meta(content: "530", name: "og:image:height"),
       script(crossorigin: "anonymous", src: "https://kit.fontawesome.com/f209982030.js"),
-      Node.raw("""
-      <script>
-      MathJax = {
-        tex: {
-          inlineMath: [['$', '$']]
-        },
-        svg: {
-          fontCache: 'global'
-        }
-      };
-      </script>
-      """),
-      script(defer: true, src: "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js")
+      Node.raw(
+        """
+        <script>
+        MathJax = {
+          tex: {
+            inlineMath: [['$', '$']]
+          },
+          svg: {
+            fontCache: 'global'
+          }
+        };
+        </script>
+        """),
+      script(defer: true, src: "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js"),
     ])
   }
 }
